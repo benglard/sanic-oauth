7c506a7d15228727ec1ae08e045b53eaf9ff85f8
Lewis Gaul <lewis.gaul@gmail.com>
2022-06-12 11:03:37 -0700

    Black format

--- examples/multi_providers.py
+++ examples/multi_providers.py
@@ -6,50 +6,51 @@ from sanic.response import text, HTTPResponse, html
 from sanic_session import InMemorySessionInterface
 from sanic_oauth.blueprint import oauth_blueprint, login_required
 
-app = Sanic('example-oauth')
+app = Sanic("example-oauth")
 app.blueprint(oauth_blueprint)
 app.session_interface = InMemorySessionInterface()
 
-app.config.OAUTH_REDIRECT_URI = 'http://127.0.0.1:8888/oauth'
-app.config.OAUTH_SCOPE = 'email'
+app.config.OAUTH_REDIRECT_URI = "http://127.0.0.1:8888/oauth"
+app.config.OAUTH_SCOPE = "email"
 app.config.OAUTH_PROVIDERS = defaultdict(dict)
-DISCORD_PROVIDER = app.config.OAUTH_PROVIDERS['discord']
-DISCORD_PROVIDER['PROVIDER_CLASS'] = 'sanic_oauth.providers.DiscordClient'
-DISCORD_PROVIDER['SCOPE'] = "identify email"
-DISCORD_PROVIDER['CLIENT_ID'] = 'insert-you-credentials'
-DISCORD_PROVIDER['CLIENT_SECRET'] = 'insert-you-credentials'
-GITLAB_PROVIDER = app.config.OAUTH_PROVIDERS['gitlab']
-GITLAB_PROVIDER['PROVIDER_CLASS'] = 'sanic_oauth.providers.GitlabClient'
-GITLAB_PROVIDER['SCOPE'] = "read_user"
-GITLAB_PROVIDER['CLIENT_ID'] = 'insert-you-credentials'
-GITLAB_PROVIDER['CLIENT_SECRET'] = 'insert-you-credentials'
-app.config.OAUTH_PROVIDERS['default'] = DISCORD_PROVIDER
+DISCORD_PROVIDER = app.config.OAUTH_PROVIDERS["discord"]
+DISCORD_PROVIDER["PROVIDER_CLASS"] = "sanic_oauth.providers.DiscordClient"
+DISCORD_PROVIDER["SCOPE"] = "identify email"
+DISCORD_PROVIDER["CLIENT_ID"] = "insert-you-credentials"
+DISCORD_PROVIDER["CLIENT_SECRET"] = "insert-you-credentials"
+GITLAB_PROVIDER = app.config.OAUTH_PROVIDERS["gitlab"]
+GITLAB_PROVIDER["PROVIDER_CLASS"] = "sanic_oauth.providers.GitlabClient"
+GITLAB_PROVIDER["SCOPE"] = "read_user"
+GITLAB_PROVIDER["CLIENT_ID"] = "insert-you-credentials"
+GITLAB_PROVIDER["CLIENT_SECRET"] = "insert-you-credentials"
+app.config.OAUTH_PROVIDERS["default"] = DISCORD_PROVIDER
 
-@app.listener('before_server_start')
+
+@app.listener("before_server_start")
 async def init_aiohttp_session(sanic_app, _loop) -> None:
     sanic_app.async_session = aiohttp.ClientSession()
 
 
-@app.listener('after_server_stop')
+@app.listener("after_server_stop")
 async def close_aiohttp_session(sanic_app, _loop) -> None:
     await sanic_app.async_session.close()
 
 
-@app.middleware('request')
+@app.middleware("request")
 async def add_session_to_request(request):
     # before each request initialize a session
     # using the client's request
     await request.app.session_interface.open(request)
 
 
-@app.middleware('response')
+@app.middleware("response")
 async def save_session(request, response):
     # after each request save the session,
     # pass the response to set client cookies
     await request.app.session_interface.save(request, response)
 
 
-@app.route('/')
+@app.route("/")
 async def main_page(_request) -> HTTPResponse:
     return html(
         """
@@ -59,20 +60,24 @@ async def main_page(_request) -> HTTPResponse:
         """
     )
 
-@app.route('/login')
+
+@app.route("/login")
 @login_required
 async def index(_request: Request, user) -> HTTPResponse:
     return text("Hello world")
 
-@app.route('/login/discord')
-@login_required(provider='discord')
+
+@app.route("/login/discord")
+@login_required(provider="discord")
 async def index(_request: Request, user) -> HTTPResponse:
     return text("Hello world")
 
-@app.route('/login/gitlab')
-@login_required(provider='gitlab')
+
+@app.route("/login/gitlab")
+@login_required(provider="gitlab")
 async def index(_request: Request, user) -> HTTPResponse:
     return text("Hello world")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(port=8888)
